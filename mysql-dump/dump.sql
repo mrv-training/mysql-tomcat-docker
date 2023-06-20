236a47a09e3b6d2368446f3bbbf315d8105ffd31
ThienMai <thienmai@magrabbit.com.vn>
2023-06-20 17:27:43 +0700

    Add project

---
+++ mysql-dump/dump.sql
@@ -0,0 +1,86 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: db
+-- Generation Time: Sep 12, 2020 at 11:53 PM
+-- Server version: 5.7.31
+-- PHP Version: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+07:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pilot-project`
+--
+
+-- --------------------------------------------------------
+
+DROP TABLE IF EXISTS `brand`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `brand` (
+  `brand_id` int(11) NOT NULL AUTO_INCREMENT,
+  `brand_name` varchar(45) DEFAULT NULL,
+  `logo` text DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  PRIMARY KEY (`brand_id`),
+  UNIQUE KEY `brand_name_UNIQUE` (`brand_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=98 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `brand`
+--
+
+LOCK TABLES `brand` WRITE;
+/*!40000 ALTER TABLE `brand` DISABLE KEYS */;
+INSERT INTO `brand` VALUES (1,'Apple','images/brand/20221124-1506-94dbck.png','Apple Inc, California'),(2,'Samsung','images/brand/20221124-1505-m6nrn9.webp','Samsung Inc, Korea'),(3,'Oppo','images/brand/20221124-1505-m0iss6.png','Oppo Inc, China'),(5,'LG','images/brand/20221124-1505-vgj6rp.png','LG Inc, Japan'),(6,'Xiaomi','images/brand/20221124-1504-ep3pdt.png','Xiaomi Inc, China'),(7,'Sony','images/brand/20221124-1504-nak19v.png','Sony Inc, Japan'),(9,'Huawei','images/brand/20221124-1503-36khjb.png','Huawei made in China 1'),(10,'Vivo','images/brand/20221124-1503-9dc89b.png','Vivo Inc, China'),(11,'HTC','images/brand/20221124-1502-d0h72d.png','HTC Inc, California'),(12,'Asus','images/brand/20221124-1501-73qa6b.svg','Asus Inc, China'),(14,'Realme','images/brand/20221124-1501-12e3hf.png','Realme Inc, China');
+/*!40000 ALTER TABLE `brand` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product` (
+  `product_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(45) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `brand_id` int(11) DEFAULT NULL,
+  `sale_date` date DEFAULT NULL,
+  `image` text DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  PRIMARY KEY (`product_id`),
+  UNIQUE KEY `product_name_UNIQUE` (`product_name`),
+  KEY `brand_id` (`brand_id`),
+  CONSTRAINT `product_ibfk_1` FOREIGN KEY (`brand_id`) REFERENCES `brand` (`brand_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=307 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,'Iphone XS Max',100,26990000,1,'2019-10-12','IPhone XS Max 128GB.jpg','Made in USA'),(2,'Iphone X',100,21090000,1,'2019-10-09','ipx.jpg','Apple\'s aim with the iPhone X was to create an iPhone.'),(3,'Iphone 8 Plus',100,17980000,1,'2019-10-09','IPhone 8 Plus 64GB.jpg','The iPhone 8 includes a 4.7-inch display.'),(4,'Iphone 7 Plus',100,16500000,1,'2019-10-10','ip7.jpg','The iPhone 7 measures in at 138.3mm tall.'),(5,'Samsung Galaxy Note 10 Plus',100,22390000,2,'2019-10-08','ss note 10+.jpg','It runs on the Samsung Exynos 9 Octa 9825 Chipset.'),(6,'Samsung Galaxy S10',100,21500000,2,'2019-10-08','Samsung Galaxy S10 128GB.jpg','The Galaxy S10 isn’t all that small, of course.'),(7,'Samsung Galaxy S10 Plus',100,21990000,2,'2019-10-08','Samsung Galaxy S10+ 2 128GB.jpg','The Galaxy S10+ is Samsung latest flagship for 2019.'),(8,'Samsung Galaxy A70',100,7990000,2,'2019-10-08','Samsung Galaxy A70 64GB.jpg','It is powered by 2GHz octa-core Qualcomm Snapdragon 675.'),(9,'Samsung Galaxy Note 9',100,20490000,2,'2019-10-08','ss note 9.jpg','Samsung Note version'),(10,'IPhone 11 Pro Max',100,42990000,1,'2019-10-08','iphone-11-pro-max-512gb-gold.jpg','New IPhone'),(11,'Iphone 11',80,21990000,1,'2019-10-08','iphone-11-128gb-purple.jpg','New version'),(12,'Iphone 6S Plus',100,8990000,1,'2019-10-12','IPhone 6 32GB.jpg','Made in USA'),(13,'Xiaomi Note 7',100,4500000,6,'2019-10-08','xiaominote7.jpg','description'),(14,'Huawei P30 Pro',120,20690000,9,'2019-10-08','huawei-p30-pro.jpg','Huawei made in China'),(15,'Huawei P30',100,15290000,9,'2019-10-08','huawei-p30-blue-600x600.jpg','Huawei made in China'),(16,'Oppo Reno 10X',70,19990000,3,'2019-10-08','oppo-reno-10x-zoom-edition-black.jpg','Oppo made in China'),(18,'Oppo A9',100,7890000,3,'2019-10-08','oppo-a9-2020-green-1-600x600.jpg','Oppo China'),(19,'Oppo A7',50,7000000,3,'2019-10-08','oppo-r17-pro-14-600x600.jpg','Oppo China');
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+-- Dump completed on 2022-12-09 18:27:51
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
